7120a104de86b1f96d37ec874f9736273ab49f2c
Shoma <beidullaevshamil@gmail.com>
2023-09-26 18:32:25 +0400

    cycle task 2

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ for(let i = 2; i <= 10; i++) {
 }
 
 
+/*Перепишите код, заменив цикл for на while, без изменения поведения цикла.
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}*/
+
+let i = 0;
+while (i < 3) {
+	alert(`number ${i}!`);
+	i++;
+}
+
+
+
